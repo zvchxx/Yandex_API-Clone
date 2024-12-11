1de2ecbe5d0b3d50da6c2a4bc4e0d8efdb17847c
zzvchx <abubakrrahmatullayev1001@gmail.com>
2024-12-12 00:28:07 +0500

    delete registerview, finish verifiyviews and loginviews

--- user/views.py
+++ user/views.py
@@ -1,7 +1,6 @@
 from random import randint
 import threading
 
-from django.contrib.auth import authenticate
 from django.utils import timezone
 
 from datetime import timedelta
@@ -12,30 +11,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 
 from user.models import UserModel
-from user.serializers import RegisterSerializers, ResendCodeSerializer, VerifyEmailSerializer, LoginSerializers, UserSerializer
+from user.serializers import ResendCodeSerializer, VerifyEmailSerializer, LoginSerializers
 from user.utils import send_email_verification
-
-
-class RegisterViews(generics.CreateAPIView):
-    serializer_class = RegisterSerializers
-    queryset = UserModel.objects.all()
-    permission_classes = [AllowAny]
-
-    def perform_create(self, serializer):
-        user = serializer.save()
-        user.set_password(serializer.validated_data['password'])
-        verification_code = str(randint(1000, 9999))  
-        user.verification_code_created_at = timezone.now() 
-        user.verification_code = verification_code
-        user.is_active = False
-        user.save()
-
-        threading.Thread(target=send_email_verification, args=(user, verification_code)).start()
-
-        return user
 
 
 class VerifiyViews(APIView):
@@ -46,12 +25,20 @@ class VerifiyViews(APIView):
         serializer = VerifyEmailSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         
-        user = serializer.validated_data['user'] 
-        user.is_active = True
+        
+        user = serializer.validated_data['user']
+        refresh = RefreshToken.for_user(user)
+        refresh.set_exp(lifetime=timedelta(minutes=30))
+        refresh.access_token.set_exp(lifetime=timedelta(minutes=15))
+        
+        user.last_login = timezone.now()
+        user.user_status = 'active'
         user.save()  
         response = {
             "success": True,
-            "message": "Email verified successfully"
+            "message": "Email verified successfully",
+            "refresh_token": str(refresh),
+            "access_token": str(refresh.access_token),
         }
 
         return Response(response, status=status.HTTP_200_OK)
@@ -65,44 +52,47 @@ class LoginView(generics.GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        username = serializer.validated_data.get('username')
         email = serializer.validated_data.get('email')
-        password = serializer.validated_data['password']
-        print(username)
-        print(password)
-        
-        user = None
-        
-        if username:  
-            user = authenticate(username=username, password=password)
-        if email and not user:
-            try:
-                user = UserModel.objects.get(email=email)
-                user = authenticate(username=user.username, password=password)
-            except UserModel.DoesNotExist:
-                user = None
-        
-        if user is not None:
-            refresh = RefreshToken.for_user(user)
-            refresh.set_exp(lifetime=timedelta(minutes=10))
-            refresh.access_token.set_exp(lifetime=timedelta(minutes=3))
-            
-            user.last_login = timezone.now()
+        full_name = serializer.validated_data.get('full_name', '')  
+
+        try:
+            user = UserModel.objects.get(email=email)
+        except UserModel.DoesNotExist:
+            user = None
+
+        if user:
+
+            verification_code = str(randint(1000, 9999))
+            user.verification_code = verification_code
+            user.verification_code_created_at = timezone.now()
+            user.user_status = 'inactive'
             user.save()
 
-            response = {
-                "refresh_token": str(refresh),
-                "access_token": str(refresh.access_token),
-                "email": user.email,
-                "username": user.username  
-            }
-            return Response(response, status=status.HTTP_200_OK)
-        
+            threading.Thread(target=send_email_verification, args=(user, verification_code)).start()
+
+            return Response({
+                "success": True,
+                "message": "Verification code sent to the existing user."
+            }, status=status.HTTP_200_OK)
+
+        user = UserModel.objects.create_user(
+            full_name=full_name,
+            email=email,
+            username=email.split('@')[0]  
+        )
+        user.user_status = 'inactive'
+        verification_code = str(randint(1000, 9999))
+        user.verification_code = verification_code
+        user.verification_code_created_at = timezone.now()
+        user.save()
+
+        threading.Thread(target=send_email_verification, args=(user, verification_code)).start()
+
         return Response({
-            "success": False,
-            "message": "Invalid credentials"
-        }, status=status.HTTP_400_BAD_REQUEST)
-    
+            "success": True,
+            "message": "New user created and verification code sent."
+        }, status=status.HTTP_201_CREATED)
+
     
 class ResendCodeView(APIView):
     def post(self, request, *args, **kwargs):
@@ -110,12 +100,3 @@ class ResendCodeView(APIView):
         if serializer.is_valid():
             return Response({"message": "Verification code has been resent successfully!"}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-
-class ProfileView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = UserSerializer
-    queryset = UserModel.objects.all()
-
-    def get_object(self):
-        return self.request.user
