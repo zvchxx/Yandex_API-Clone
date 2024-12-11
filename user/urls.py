bc6dc342020e200f09ebb1e3ca593cf5f95ae2f6
zzvchx <abubakrrahmatullayev1001@gmail.com>
2024-12-12 00:26:57 +0500

    delete register, profile

--- user/urls.py
+++ user/urls.py
@@ -5,8 +5,6 @@ from user import views
 
 urlpatterns = [
     path('login/', views.LoginView.as_view()),  
-    path('register/', views.RegisterViews.as_view()),  
     path('verify-email/', views.VerifiyViews.as_view()),  
     path('resend-code/', views.ResendCodeView.as_view()),
-    path('profile/', views.ProfileView.as_view()),
 ]
