917f43c8547721cd3ec8c264824c3e288a239e25
zzvchx <abubakrrahmatullayev1001@gmail.com>
2024-12-14 00:57:30 +0500

    add BranchModel

--- branch/models.py
+++ branch/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.utils.translation import gettext_lazy as _
 
 from restaurant.models import RestaurantModel
 
@@ -9,24 +8,25 @@ User = get_user_model()
 
 
 class BranchModel(models.Model):
-    name = models.CharField(max_length=64, verbose_name=_("Name"), unique=True)
-    address = models.CharField(max_length=255, verbose_name=_("Address"))
-    phone_number = models.CharField(max_length=255, verbose_name=_("Phone Number"))
+    name = models.CharField(max_length=64, verbose_name=("Name"), unique=True)
+    address = models.CharField(max_length=255, verbose_name=("Address"))
+    phone_number = models.CharField(max_length=255, verbose_name=("Phone Number"))
     longitude = models.FloatField()
     latitude = models.FloatField()
     manager = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
+        null=True, blank=True,
         related_name="branches",
-        verbose_name=_("Manager")
+        verbose_name=("Manager")
     )
     restaurant = models.ForeignKey(
         RestaurantModel,
         on_delete=models.CASCADE,
         related_name="branches",
-        verbose_name=_("Restaurant")
+        verbose_name=("Restaurant")
     )
-    is_active = models.BooleanField(default=True, verbose_name=_("Is Active"))
+    is_active = models.BooleanField(default=True, verbose_name=("Is Active"))
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -37,5 +37,5 @@ class BranchModel(models.Model):
 
     class Meta:
         ordering = ['-id']
-        verbose_name = _("Branch")
-        verbose_name_plural = _("Branches")
+        verbose_name = ("Branch")
+        verbose_name_plural = ("Branches")
