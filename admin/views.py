e220ff065f23bb822d87bd2e0fd8148096fdfbd7
zzvchx <abubakrrahmatullayev1001@gmail.com>
2024-12-14 00:48:38 +0500

    add ManagerViewSet and CourierViewSet

--- admin/views.py
+++ admin/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from user.models import UserModel
+
+from admin.serializers import ManagerSerializer, CourierSerializer
+
+
+class ManagerViewSet(ModelViewSet):
+    queryset = UserModel.objects.all()
+    serializer_class = ManagerSerializer
+    permission_classes = [IsAdminUser]
+
+
+class CourierViewSet(ModelViewSet):
+    queryset = UserModel.objects.all()
+    serializer_class = CourierSerializer
+    permission_classes = [IsAdminUser]
