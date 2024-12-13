e889a6c6a38daea382ec2a59a0830fad773cf1ee
zzvchx <abubakrrahmatullayev1001@gmail.com>
2024-12-14 00:58:29 +0500

    adit project

--- Conf/settings.py
+++ Conf/settings.py
@@ -139,6 +139,7 @@ REST_FRAMEWORK = {
 
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
--- Conf/urls.py
+++ Conf/urls.py
@@ -3,12 +3,23 @@ from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/user/', include('user.urls')),  
-    path('api/product/', include('product.urls')),
-    path('api/feedback/', include('feedback.urls')),
+    path('superadmin/', admin.site.urls),
+    path('admin/', include('admin.urls')), 
+    path('user/', include('user.urls')), 
+    path('branch/', include('branch.urls')), 
+    path('courier/', include('courier.urls')), 
+    path('restaurant/', include('restaurant.urls')),  
+    path('product/', include('product.urls')),
+    path('feedback/', include('feedback.urls')),
+]
+
+urlpatterns += [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
