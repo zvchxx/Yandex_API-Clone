b5b7408aeb0df0a65da81ba6615279569cb7de05
zzvchx <abubakrrahmatullayev1001@gmail.com>
2024-12-16 02:34:32 +0500

    edit views

--- branch/views.py
+++ branch/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from branch.permissions import IsManager
+from branch.models import BranchModel
+from branch.serializers import BranchSerializer
+from branch.paginations import CustomPagination
+
+class BranchViewSet(ModelViewSet):
+    queryset = BranchModel.objects.all()
+    serializer_class = BranchSerializer
+    permission_classes = [IsAuthenticated, IsManager]
+    pagination_class = CustomPagination
